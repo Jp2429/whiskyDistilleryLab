5ea5e1b4001fa093f2b0c4c5932efd31e4c19c21
John Panton <johnpanton29@outlook.com>
2023-03-08 14:41:19 +0000

    first commit

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -0,0 +1,36 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class WhiskyController {
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping(value="/whiskies")
+    public ResponseEntity<List<Whisky>> getAllWhiskies(
+            @RequestParam(name="year", required=false) Integer year,
+            @RequestParam(name="age",required=false) Integer age,
+            @RequestParam(name="name",required = false) String name,
+            @RequestParam(name="region",required = false) String region
+    ){
+        if(year!=null){
+            return new ResponseEntity<>(whiskyRepository.findByYear(year), HttpStatus.OK);
+        }
+        if(name!=null && age!=null){
+            return new ResponseEntity<>(whiskyRepository.findByDistilleryNameAndAge(name,age),HttpStatus.OK);
+        }
+        if(region!=null){
+            return new ResponseEntity<>(whiskyRepository.findByDistilleryRegion(region),HttpStatus.OK);
+        }
+        return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+    }
+
+}
